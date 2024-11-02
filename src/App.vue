8b0adc2f48615de689bf69997ff66cb2dbdee26e
Benmammar Rayan <rayan.benmammar90@gmail.com>
2024-11-02 13:29:17 +0100

    init project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <h1>VueJS Product Management App</h1>
+  <RouterView />
+</template>
+
+<style scoped></style>
