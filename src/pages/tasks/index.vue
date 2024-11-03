9ad4177b77b2b3e1c2da5cbc685e7e6b203fedfd
Benmammar Rayan <rayan.benmammar90@gmail.com>
2024-11-03 13:03:49 +0100

    added tasks : db migration + seeding + types

---
+++ src/pages/tasks/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+import type { Tables } from '../../../database/types'
+
+const tasks = ref<Tables<'tasks'>[] | null>(null)
+;(async () => {
+  const { data, error } = await supabase.from('tasks').select()
+
+  if (error) return console.error(error)
+
+  tasks.value = data
+})()
+</script>
+
+<template>
+  <div>
+    <h1>Tasks Page</h1>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped></style>
--- typed-router.d.ts
+++ typed-router.d.ts
@@ -22,5 +22,6 @@ declare module 'vue-router/auto-routes' {
     '/[..catchAll]': RouteRecordInfo<'/[..catchAll]', '/:atchAll(.*)', { atchAll: ParamValue<true> }, { atchAll: ParamValue<false> }>,
     '/projects/': RouteRecordInfo<'/projects/', '/projects', Record<never, never>, Record<never, never>>,
     '/projects/[id]': RouteRecordInfo<'/projects/[id]', '/projects/:id', { id: ParamValue<true> }, { id: ParamValue<false> }>,
+    '/tasks/': RouteRecordInfo<'/tasks/', '/tasks', Record<never, never>, Record<never, never>>,
   }
 }
