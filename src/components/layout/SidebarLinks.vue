a9bcdf4885b2d06bd177fa3636e4bc71373cbfdb
Benmammar Rayan <rayan.benmammar90@gmail.com>
2024-11-03 14:55:37 +0100

    added SidebarLinks component + refactor sidebar.vue

---
+++ src/components/layout/SidebarLinks.vue
@@ -0,0 +1,27 @@
+<script lang="ts">
+interface LinkProps {
+  title: string
+  to: string
+  icon: string
+}
+</script>
+
+<script setup lang="ts">
+const props = defineProps<{
+  links: LinkProps[]
+}>()
+</script>
+
+<template>
+  <RouterLink
+    v-for="link in props.links"
+    :key="link.title"
+    :to="link.to"
+    class="flex items-center gap-3 px-4 py-2 mx-2 transition-colors rounded-lg hover:text-primary justify-center lg:justify-normal text-muted-foreground"
+  >
+    <iconify-icon :icon="link.icon"></iconify-icon>
+    <span class="hidden lg:block text-nowrap">{{ link.title }}</span>
+  </RouterLink>
+</template>
+
+<style scoped></style>
