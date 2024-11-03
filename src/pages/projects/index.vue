dc118ec6420b391b23e07c052bfe86c0b2c11068
Benmammar Rayan <rayan.benmammar90@gmail.com>
2024-11-03 12:03:47 +0100

    retrieve projects from DB

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,4 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+
+let projects = ref()
+;(async () => {
+  const { data, error } = await supabase
+    .from('projects')
+    .select('id, name, slug, status, collaborators')
+    .order('created_at', { ascending: false })
+
+  if (error) return console.error(error)
+
+  projects.value = data
+})()
+</script>
 
 <template>
   <div>
