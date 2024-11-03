165cb4e9f699f73f3c64131e0a3d14e190d5f751
rayan <rayan.benmammar90@gmail.com>
2024-11-04 00:53:21 +0100

    Using Pinia for Efficient Data Loading and Caching

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -5,14 +5,9 @@ import type { Projects } from '@/utils/supaQueries'
 
 usePageStore().pageData.title = 'Projects'
 
-const projects = ref<Projects | null>(null)
-const getProjects = async () => {
-  const { data, error, status } = await projectsQuery
-
-  if (error) useErrorStore().setError({ error, customCode: status })
-
-  projects.value = data
-}
+const projectsLoader = useProjectsStore()
+const { projects } = storeToRefs(projectsLoader)
+const { getProjects } = projectsLoader
 await getProjects()
 </script>
 
