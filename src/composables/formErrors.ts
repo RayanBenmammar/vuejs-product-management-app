cee69e828d24db0d5089fe2e423906a67fb6fa4c
rayan <rayan.benmammar90@gmail.com>
2024-11-04 00:30:22 +0100

    TypeScript Mapping and Generics

--- src/composables/formErrors.ts
+++ src/composables/formErrors.ts
@@ -1,9 +1,13 @@
 import type { LoginForm } from '@/types/AuthForm'
 import type { AuthError } from '@supabase/supabase-js'
 
+type FormErrors<Type> = {
+  [Property in keyof Type]: string[]
+}
+
 export const useFormErrors = () => {
   const serverError = ref('')
-  const realTimeErrors = ref()
+  const realTimeErrors = ref<FormErrors<LoginForm>>()
 
   const handleServerError = (error: AuthError) => {
     serverError.value =
