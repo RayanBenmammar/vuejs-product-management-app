6cc59cc617eaeb6f3b8055ae91727a2108e354f0
Benmammar Rayan <rayan.benmammar90@gmail.com>
2024-11-03 22:50:18 +0100

    update task schema and seed

--- database/seed.js
+++ database/seed.js
@@ -4,6 +4,12 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.VITE_SUPABASE_URL
 const serviceRoleKey = process.env.SERVICE_ROLE_KEY
 const supabase = createClient(supabaseUrl, serviceRoleKey)
+const testingUserEmail = process.env.TESTING_USER_EMAIL
+if (!testingUserEmail) {
+  console.error('Have you forgot to add TESTING_USER_EMAIL to your .env file?')
+  process.exit()
+}
+
 const logErrorAndExit = (tableName, error) => {
   console.error(
     `An error occurred in table '${tableName}' with code ${error.code}: ${error.message}`,
@@ -15,7 +21,62 @@ const logStep = stepMessage => {
   console.log(stepMessage)
 }
 
-const seedProjects = async numEntries => {
+const PrimaryTestUserExists = async () => {
+  logStep('Checking if primary test user exists...')
+  const { data, error } = await supabase
+    .from('profiles')
+    .select('id, username')
+    .eq('username', 'testaccount1')
+    .single()
+
+  if (error) {
+    console.log('Primary test user not found. Will create one.')
+    return false
+  }
+
+  logStep('Primary test user is found.')
+  return data?.id
+}
+
+const createPrimaryTestUser = async () => {
+  logStep('Creating primary test user...')
+  const firstName = 'Test'
+  const lastName = 'Account'
+  const userName = 'testaccount1'
+  const email = testingUserEmail
+  const { data, error } = await supabase.auth.signUp({
+    email: email,
+    password: 'password',
+    options: {
+      data: {
+        first_name: firstName,
+        last_name: lastName,
+        full_name: firstName + ' ' + lastName,
+        username: userName,
+      },
+    },
+  })
+
+  if (error) {
+    logErrorAndExit('Users', error)
+  }
+
+  if (data) {
+    const userId = data.user.id
+    await supabase.from('profiles').insert({
+      id: userId,
+      full_name: firstName + ' ' + lastName,
+      username: userName,
+      bio: 'The main testing account',
+      avatar_url: `https://i.pravatar.cc/150?u=${data.user.id}`,
+    })
+
+    logStep('Primary test user created successfully.')
+    return userId
+  }
+}
+
+const seedProjects = async (numEntries, userId) => {
   logStep('Seeding projects...')
   const projects = []
 
@@ -25,9 +86,9 @@ const seedProjects = async numEntries => {
     projects.push({
       name: name,
       slug: name.toLocaleLowerCase().replace(/ /g, '-'),
-      description: faker.lorem.paragraph(2),
+      description: faker.lorem.paragraphs(2),
       status: faker.helpers.arrayElement(['in-progress', 'completed']),
-      collaborators: faker.helpers.arrayElements([1, 2, 3]),
+      collaborators: faker.helpers.arrayElements([userId]),
     })
   }
 
@@ -43,7 +104,7 @@ const seedProjects = async numEntries => {
   return data
 }
 
-const seedTasks = async (numEntries, projectsIds) => {
+const seedTasks = async (numEntries, projectsIds, userId) => {
   logStep('Seeding tasks...')
   const tasks = []
 
@@ -53,8 +114,9 @@ const seedTasks = async (numEntries, projectsIds) => {
       status: faker.helpers.arrayElement(['in-progress', 'completed']),
       description: faker.lorem.paragraph(),
       due_date: faker.date.future(),
+      profile_id: userId,
       project_id: faker.helpers.arrayElement(projectsIds),
-      collaborators: faker.helpers.arrayElements([1, 2, 3]),
+      collaborators: faker.helpers.arrayElements([userId]),
     })
   }
 
@@ -71,10 +133,21 @@ const seedTasks = async (numEntries, projectsIds) => {
 }
 
 const seedDatabase = async numEntriesPerTable => {
-  const projectsIds = (await seedProjects(numEntriesPerTable)).map(
+  let userId
+
+  const testUserId = await PrimaryTestUserExists()
+
+  if (!testUserId) {
+    const primaryTestUserId = await createPrimaryTestUser()
+    userId = primaryTestUserId
+  } else {
+    userId = testUserId
+  }
+
+  const projectsIds = (await seedProjects(numEntriesPerTable, userId)).map(
     project => project.id,
   )
-  await seedTasks(numEntriesPerTable, projectsIds)
+  await seedTasks(numEntriesPerTable, projectsIds, userId)
 }
 
 const numEntriesPerTable = 10
