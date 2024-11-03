8f54469f50fa14ec0cd387671f25f6ee9da9d1d3
Benmammar Rayan <rayan.benmammar90@gmail.com>
2024-11-03 19:32:08 +0100

    adjust error page for native js errors

--- src/stores/error.ts
+++ src/stores/error.ts
@@ -3,22 +3,22 @@ import type { PostgrestError } from '@supabase/supabase-js'
 
 export const useErrorStore = defineStore('error-store', () => {
   const activeError = ref<null | CustomError | ExtendedPostgrestError>(null)
+
   const setError = ({
     error,
     customCode,
   }: {
-    error: string | PostgrestError
-    customCode: number
+    error: string | PostgrestError | Error
+    customCode?: number
   }) => {
-    if (typeof error === 'string') {
-      activeError.value = Error(error)
-      activeError.value.customCode = customCode
+    if (typeof error === 'string' || error instanceof Error) {
+      activeError.value = typeof error === 'string' ? Error(error) : error
+      activeError.value.customCode = customCode || 500
       return
     }
-    console.log(error)
-    console.log(customCode)
+
     activeError.value = error
-    activeError.value.statusCode = customCode
+    activeError.value.statusCode = customCode || 500
   }
 
   return {
