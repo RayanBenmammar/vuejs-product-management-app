2e01ad895d5981050d03f8e0178478bbad4c878c
Benmammar Rayan <rayan.benmammar90@gmail.com>
2024-11-03 16:20:24 +0100

    added unplugin-auto-import plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,29 @@
 import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 
-import autoprefixer from 'autoprefixer'
 import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { compile } from 'vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()],
-    },
-  },
   plugins: [
     VueRouter(),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/, // .md
+      ],
+      imports: ['vue', VueRouterAutoImports],
+      dts: true,
+      viteOptimizeDeps: true,
+    }),
     vue({
       template: {
         compilerOptions: {
@@ -25,6 +32,11 @@ export default defineConfig({
       },
     }),
   ],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
