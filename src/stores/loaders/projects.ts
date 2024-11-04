018ec352348857a88de8cab3d609ad14b87b8998
rayan <rayan.benmammar90@gmail.com>
2024-11-04 01:01:38 +0100

    Implement Stale While Revalidate with Pinia and useMemoize

--- src/stores/loaders/projects.ts
+++ src/stores/loaders/projects.ts
@@ -5,6 +5,15 @@ export const useProjectsStore = defineStore('projects-store', () => {
   const projects = ref<Projects | null>(null)
   const loadProjects = useMemoize(async (key: string) => await projectsQuery)
   const getProjects = async () => {
+    if (projects.value?.length) {
+      projectsQuery.then(({ data }) => {
+        if (JSON.stringify(data) !== JSON.stringify(projects.value)) {
+          return
+        } else {
+          loadProjects.delete('projects')
+        }
+      })
+    }
     const { data, error, status } = await loadProjects('projects')
 
     if (error) useErrorStore().setError({ error, customCode: status })
